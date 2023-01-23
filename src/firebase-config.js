a83c10d18d6afa914a5f403de69af442c87f1741
MazkaAndrii <andriy.mazka@gmail.com>
2023-01-23 15:54:46 +0200

    change firebaseconfig

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDmJ4-dsNeYfju7UGbKCBelQu0yDTY7HvQ',
-  authDomain: 'filmoteka-project-692be.firebaseapp.com',
-  projectId: 'filmoteka-project-692be',
-  storageBucket: 'filmoteka-project-692be.appspot.com',
-  messagingSenderId: '387583714893',
-  appId: '1:387583714893:web:72ce0f43106b12031d9e04',
+  apiKey: 'AIzaSyBM7qZzHkWQksciEPD3_K4ysG_DoJGpr4k',
+  authDomain: 'filmoteka-9f4e7.firebaseapp.com',
+  projectId: 'filmoteka-9f4e7',
+  storageBucket: 'filmoteka-9f4e7.appspot.com',
+  messagingSenderId: '746113909904',
+  appId: '1:746113909904:web:ce56e139f2779fc7300415',
 };
 
 export const app = initializeApp(firebaseConfig);
